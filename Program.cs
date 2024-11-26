a939572e7a3e20347ffde9750108a73618f18313
unknown <politia444@gmail.com>
2024-11-26 22:15:10 +0900

    if문

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+class MainClass {
+    public static void Main(string[] args){
+        int num = 8;
+
+        if (num == 10) Console.WriteLine("입력 값은 10 입니다.");
+        else Console.WriteLine("입력 값은 10 아닙니다.");
+    }
+}
--- obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed632e0a1e7e9da0fc96c244b2fa8ffcd947135")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
